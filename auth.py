23580d00ab8799d1c4480fa720f75ee902398d24
Tiziano Zito <opossumnano@gmail.com>
2023-06-26 10:39:59 -0600

    protect execution of get_credentials

--- auth.py
+++ auth.py
@@ -3,4 +3,12 @@ def get_credentials():
     password = input('Type password: ')
     return user, password
 
-get_credentials()
+#def write_pwdb():
+
+#def read_pwdb():
+
+#def add_user():
+
+if __name__ == '__main__':
+    get_credentials()
+

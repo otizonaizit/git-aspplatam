aa27b5d7f2a86d4696d83981aa8c58edc9bd57a4
Jakob Jordan <jakobjordan@posteo.de>
2023-06-26 12:53:34 -0600

    Add function to store database

--- auth.py
+++ auth.py
@@ -1,14 +1,21 @@
+import json
+
 def get_credentials():
     user = input('Type user name: ')
     password = input('Type password: ')
     return user, password
 
-#def write_pwdb():
+def write_pwdb(user, password):
+    pwdb = {}
+    pwdb[user] = password
+    fn = 'database.json'
+    with open(fn, 'w') as f:
+        json.dump(pwdb, f)
 
 #def read_pwdb():
 
 #def add_user():
 
 if __name__ == '__main__':
-    get_credentials()
-
+    user, password = get_credentials()
+    write_pwdb(user, password)
